220173511843fd511c99d3542979720945e8019d
ghostrider07008 <ghostrider07008@gmail.com>
2019-04-04 16:49:12 +0300

    написал код добавления товаров в корзину

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+let f = () => {
+const goods = [
+    {title: 'Shirt', price: '150 &#8381;'},
+    {title: 'Socks', price: '50 &#8381;'},
+    {title: 'Jacket', price: '350 &#8381;'},
+    {title: 'Shoes', price: '250 &#8381;'},
+    ]
+    const renderGoodsItem = (title, price) => {
+        return `<div class="goods-item"><h2>${title}</h2><p>${price}</div>`;
+    }
+
+    const renderGoodsList = (list) => {
+        let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+        document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    }
+    renderGoodsList(goods);
+}
