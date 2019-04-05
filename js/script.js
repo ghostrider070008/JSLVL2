684c1275e05496a3b86062c077fe4c7e8b8be0ca
ghostrider07008 <ghostrider07008@gmail.com>
2019-04-05 08:49:59 +0300

    наполнение товарами

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 "use strict";
 const goods = [
-    {title: 'Shirt', price: '150 &#8381;'},
-    {title: 'Socks', price: '50 &#8381;'},
-    {title: 'Jacket', price: '350 &#8381;'},
-    {title: 'Shoes', price: '250 &#8381;'},
-    ]
+    {title: 'Ноутбук Asus X540UA-DM597T', price: '31200&#8381;', src: '../img/catalog/nout_asusx540UA.jpg'},
+    {title: 'Ноутбук Asus X756UA-T4613D', price: '34300&#8381;', src: '../img/catalog/nout_asusx756ua.jpg'},
+    {title: 'Ноутбук Asus X405UA-EB920T', price: '34300&#8381;', src: '../img/catalog/nout_asusx405UA.jpg'},
+    {title: 'Ноутбук Asus X405UA-EB920T', price: '34300&#8381;', src: '../img/catalog/nout_asusx405UA.jpg'},
+    ];``
     const renderGoodsItem = (title, price) => {
-        return `<div class="goods-item"><img><h2>${title}</h2><p>${price}</div>`;
-    }
+        return `<div class="goods-item"><h2>${title}</h2><p>${price}</div>`;
+    };
 
     const renderGoodsList = (list) => {
         let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-        document.querySelector('.goods-list').innerHTML = goodsList.join('');
+        document.querySelector('.goods-list').innerHTML = goodsList;
     }
-    renderGoodsList(goods);
+    //renderGoodsList(goods);
