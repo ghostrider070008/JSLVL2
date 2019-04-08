71884285752a45cc32d523beb1da9f4bea529f9c
ghostrider07008 <ghostrider07008@gmail.com>
2019-04-08 16:56:44 +0300

    Начал делать ДЗ

---
+++ js/cart.js
@@ -0,0 +1,55 @@
+"use strict";
+/*
+let cart = [], i = 0;
+class CartGoods {
+    constructor (number, title, price, piece){
+        this.number = number;
+        this.title = title;
+        this.price = price;
+        this.piece = piece;
+    }
+}
+let number = 1, title = 'Ноутбук Asus X540UA-DM597T', price = 31200, piece = 1;
+let GoodsCart = new CartGoods(number, title, price, piece);
+console.log(GoodsCart);
+cart[i] = GoodsCart;
+i++;
+cart[i] = GoodsCart;
+console.log(cart);
+*/
+// Класс со списком товаров
+class GoodsListClass{
+    /*
+    constructor (title, price, src){
+        this.title = title;
+        this.price = price;
+        this.src = src;
+    }*/
+    constructor (){
+        this.goods = [];
+    }
+    addGoods(title, price, src){
+       let id = this.goods.length;
+        this.goods[id] = {title, price, src};
+    }
+}
+// Класс со списком корзины
+class CartList{
+    constructor() {
+        this.goods = [];
+    }
+   addGoods(title, price, pice) {
+    
+   // }
+}
+}
+// основная программа
+
+let GoodsList = new GoodsListClass();
+    GoodsList.addGoods('Ноутбук Asus X540UA-DM597T', 31200, 'img/123.jpg');
+    GoodsList.addGoods('Ноутбук Asus X540UA-DM597T', 31200, 'img/123.jpg');
+
+//let NewGoods = new GoodsList('Ноутбук Asus X540UA-DM597T', 31200, 'img/123.jpg');
+let cart = new CartList();
+console.log(cart);
+console.log(GoodsList);
