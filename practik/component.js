4e88732ad63ddb7138b1e9ea4b6bbcb7e510a257
ghostrider070008 <ghostrider07008@yandex.ru>
2019-04-25 22:00:51 +0300

    Методичка

---
+++ practik/component.js
@@ -0,0 +1,26 @@
+// GLOBAL
+
+/*
+Vue.component ('el-comp', {
+    template: `
+    <div>
+    <h1>Hi! {{ name }} <slot></slot><another-component></anoter-component></h1>
+    <div>
+    `,
+    data () {
+        return {
+            name: 'Frodo'
+        }
+    }
+});
+
+Vue.component('another-component', {
+    template: '<i></i>'
+  });
+  */
+
+ Vue.component('el-comp', {
+     props: ['name'],
+    template: '<h1>{{ name }}</h1>'
+  });
+  
