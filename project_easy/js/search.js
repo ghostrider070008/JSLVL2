ad31d5488a96eaaed89da725609051d4f413c183
ghostrider07008 <ghostrider07008@gmail.com>
2019-04-26 12:02:20 +0300

    начал ДЗ

---
+++ project_easy/js/search.js
@@ -0,0 +1,11 @@
+Vue.component('search',{
+    props: ['userSearch'],
+    template: `
+    <form action="#" class="search-form" @submit.prevent="$emit('filter', userSearch)">
+                <input type="text" class="search-field" v-model="userSearch">
+                <button class="btn-search" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+            `
+}) 
